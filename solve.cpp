6deac4ad5f237fbc4f10c985afdd0b47b84fb8be
Mina Mourice Mohareb <minamourice11@yahoo.com>
2017-11-25 23:22:03 +0200

    Add Solve.cpp (Contain Functions Prototype)

---
+++ solve.cpp
@@ -0,0 +1,8 @@
+#include "cmatrix.h"
+#include <cmath>
+
+string solve(string s, string varNames[100], string varContent[100], int& variablesNo) {} // Hanaa /* Takes a string as input and returns a string output, Input may contain previous predefined variables and/or normal numbers */
+
+CMatrix solveCMatrix(string s, string varNames[100], string varContent[100] , int& variablesNo, int& outFileSize) {} // Verina /* Takes a string as input and returns a matrix output, Input may contain previous predefined variables and/or normal numbers */
+
+double solveDouble(string s, string varNames[100], string varContent[100] , int& variablesNo, int& outFileSize) {} //  Marina /* Takes a string as input and returns a double output, Input may contain previous predefined variables and/or normal numbers */
