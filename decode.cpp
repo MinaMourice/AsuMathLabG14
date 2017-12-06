b795a987c5c728707033ceded4ac4bb3251c55db
BeshoyAnwar <32778960+BeshoyAnwar@users.noreply.github.com>
2017-12-06 22:13:35 +0200

    Update decode.cpp

--- decode.cpp
+++ decode.cpp
@@ -1,29 +1,29 @@
-void print(string s, string varNames[100], string varContent[100], int variableNo) {
+void print(string s, string varNames[100], string varContent[100], int& variableNo) {
 	for (int index = 0; index < variableNo; index++) {
 		if (varNames[index] == s) {
-			cout << varNames << "=" << varContent[index].getString();
+			cout << varNames[index] << "=" << varContent[index]<<endl;
 			return;
 		}
 	}
-
+	cout << s << " is not defined"<< endl;
 }
-void decode(string info, string varNames[100], string varContent[100],int& variableNo) {
+void decode(string info, string varNames[100], string varContent[100], int& variableNo) {
 	int defMatFlag = 0;
 	for (int charIndex = 0; charIndex < info.length(); charIndex++) {
 		if (info[charIndex] == '[') {
 			defMatFlag = 1;
-			//defMat(info, varNames, varContent, variableNo);
+			defMat(info, varNames, varContent, variableNo);
 			cout << "send: " << info << "to defMat" << endl;
 			break;
 		}
 	}
 	if (defMatFlag == 0) {
 		if (info.find("rand") != string::npos || info.find("eye") != string::npos || info.find("ones") != string::npos || info.find("zeros") != string::npos) {
-			//defSpecialMat(info, varNames, varContent, variableNo);
+			defSpecialMat(info, varNames, varContent, variableNo);
 			cout << "send: " << info << "to defSpecialMat" << endl;
 		}
-		else if(info.find('=') != string::npos){
-			//doOperation(info, varNames, varContent, variableNo);
+		else if (info.find('=') != string::npos) {
+			doOperation(info, varNames, varContent, variableNo);
 			cout << "send: " << info << "to doOperation" << endl;
 		}
 		else {
