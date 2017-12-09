0f8a939e9a6c2f9d857acbca72259c14768b29b6
verina-alber <33982522+verina-alber@users.noreply.github.com>
2017-12-09 12:19:03 +0200

    Update solve.cpp

--- solve.cpp
+++ solve.cpp
@@ -1,146 +1,161 @@
 CMatrix solveCMatrix(string s, string varNames[100], string varContent[100] , int& variablesNo, int& outFileSize)  // Verina /* Takes a string as input and returns a matrix output, Input may contain previous predefined variables and/or normal numbers */
-	{
-		string operand1 , operand2 ;  
+{
+	string operand1 , operand2 ;  CMatrix valueOfOperand1 , valueOfOperand2 ;  string numberString ;
 
-		int placeOfOPerator = s.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_") ;
-		if (placeOfOPerator != std::string::npos)
+	int placeOfOPerator = s.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_") ;
+	if (placeOfOPerator != std::string::npos)
 		{
-			
 			if((s[placeOfOPerator])==40)                        // (
 			{
-			    int p = s.find(")");
+				int p = s.find(")");
 
 				if (s.substr(0,placeOfOPerator)!="pow")
 				{
 					int operand2index = distance( varNames, find( varNames , varNames + 100 , s.substr(placeOfOPerator+1,p-placeOfOPerator-1) ) ); 
-			    	CMatrix valueOfOperand2 = CMatrix (varContent[operand2index]);
+			    	valueOfOperand2 = CMatrix (varContent[operand2index]); 
 				}
-				
 				
 				if(s.substr(0,placeOfOPerator)=="sin")          return sin   (valueOfOperand2) ;
 				else if(s.substr(0,placeOfOPerator)=="cos")     return cos   (valueOfOperand2) ;
 				else if(s.substr(0,placeOfOPerator)=="tan")     return tan   (valueOfOperand2) ;
+				else if(s.substr(0,placeOfOPerator)=="sec")     return sec   (valueOfOperand2) ;
+				else if(s.substr(0,placeOfOPerator)=="csc")     return csc   (valueOfOperand2) ;
+				else if(s.substr(0,placeOfOPerator)=="cot")     return cot   (valueOfOperand2) ;
+				//else if(s.substr(0,placeOfOPerator)=="sinh")     return sinh  (valueOfOperand2) ;
+				//else if(s.substr(0,placeOfOPerator)=="cosh")     return cosh  (valueOfOperand2) ;
+				//else if(s.substr(0,placeOfOPerator)=="tanh")     return tanh  (valueOfOperand2) ;
+				//else if(s.substr(0,placeOfOPerator)=="tanh")     return sech  (valueOfOperand2) ;
+				//else if(s.substr(0,placeOfOPerator)=="tanh")     return csch   (valueOfOperand2) ;
+				//else if(s.substr(0,placeOfOPerator)=="tanh")     return coth  (valueOfOperand2) ;
+
 				else if(s.substr(0,placeOfOPerator)=="asin")    return asin  (valueOfOperand2) ;
 				else if(s.substr(0,placeOfOPerator)=="acos")    return acos  (valueOfOperand2) ;
 				else if(s.substr(0,placeOfOPerator)=="atan")    return atan  (valueOfOperand2) ;
-				else if(s.substr(0,placeOfOPerator)=="sqrt")    return sqrt  (valueOfOperand2) ;
+				//else if(s.substr(0,placeOfOPerator)=="sqrt")    return sqrt  (valueOfOperand2) ;
 				else if(s.substr(0,placeOfOPerator)=="exp")     return exp   (valueOfOperand2) ;
 				else if(s.substr(0,placeOfOPerator)=="log")     return log   (valueOfOperand2) ;
 				else if(s.substr(0,placeOfOPerator)=="log10")   return log10 (valueOfOperand2) ;
-				else if(s.substr(0,placeOfOPerator)=="pow")    
+
+				/*else if(s.substr(0,placeOfOPerator)=="pow")    
 					{
-						size_t const findDouble = s.find_first_of("0123456789");
+						int findDouble = s.find_first_of("0123456789");
 						if (findDouble != std::string::npos)
 						{
-							size_t const endDouble = s.find_first_not_of("0123456789", findDouble);
-							double valueOfOperand2 = stod ( s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble) );
+							int endDouble = s.find_first_not_of("0123456789", findDouble);
+							valueOfOperand2 = CMatrix (s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble) ) ;
 						}
 
 						int operand1index = distance( varNames, find( varNames , varNames + 100 , s.substr(0,placeOfOPerator-3) ) );
-				        CMatrix valueOfOperand1 = CMatrix (varContent[operand1index]);
+				        valueOfOperand1 = CMatrix (varContent[operand1index]);
 
-				        return valueOfOperand1 pow ( valueOfOperand2 ) ;
-				    }
-
+				        return valueOfOperand1 . pow ( valueOfOperand2 ) ;
+				    }*/
 			}
-						
+
+	
 			else if(s[placeOfOPerator]==46)                      // .
 			{
-				size_t const findDouble = s.find_first_of("0123456789");  
-		{
+				int findDouble = s.find_first_of("0123456789");   
 				if (findDouble != std::string::npos)
 		         {
-					size_t const endDouble = s.find_first_not_of("0123456789", findDouble);
-					if(findDouble<placeOfOPerator)
+					 int endDouble = s.find_first_not_of("0123456789", findDouble);
+					 //cout<<s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble);
+					 if(findDouble<placeOfOPerator)
 						{
-							double valueOfOperand1 = stod ( s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble) );
+							numberString = stod (s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble)) ;
+							valueOfOperand1 = CMatrix (numberString  ) ;
 							int operand2index = distance( varNames, find( varNames , varNames + 100 , s.substr(placeOfOPerator+2,s.length()) ) );
-				            CMatrix valueOfOperand2 = CMatrix (varContent[operand2index]);
+				            valueOfOperand2 = CMatrix (varContent[operand2index]);
 					    }
 					else
 						{
-							double valueOfOperand2 = stod ( s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble) );
+							numberString = s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble) ;
+							valueOfOperand2 = CMatrix ("[" + numberString + "]") ;
 							int operand1index = distance( varNames, find( varNames , varNames + 100 , s.substr(0,placeOfOPerator) ) ); 
-				            CMatrix valueOfOperand1 = CMatrix (varContent[operand1index]);
+				            valueOfOperand1 = CMatrix (varContent[operand1index]); 
 					    }
 
-				}
+				 }
  
 				else 
 				{
 					int operand1index = distance( varNames, find( varNames , varNames + 100 , s.substr(0,placeOfOPerator) ) ); 
-				    CMatrix valueOfOperand1 = CMatrix (varContent[operand1index]);					
-				
+					int foundMatrix1 = varContent [operand1index] . find ("[");
+					if(foundMatrix1 != std::string::npos)    valueOfOperand1 = CMatrix (varContent[operand1index]);
+					else 	 valueOfOperand1 = CMatrix ("[" + varContent[operand1index] + "]");
+
 					int operand2index = distance( varNames, find( varNames , varNames + 100 , s.substr(placeOfOPerator+2,s.length()) ) );
-				    CMatrix valueOfOperand2 = CMatrix (varContent[operand2index]);
+				    int foundMatrix2 = varContent [operand2index] . find ("[");
+					if(foundMatrix2 != std::string::npos)    valueOfOperand2 = CMatrix (varContent[operand2index]);
+					else 	 valueOfOperand2 = CMatrix ("[" + varContent[operand2index] + "]");
 				}
-     	}
-				
-				if(s[placeOfOPerator]==42)        return valueOfOperand1 .* valueOfOperand2 ;
-				else if(s[placeOfOPerator]==47)   return valueOfOperand1 ./ valueOfOperand2 ;
-				else if(s[placeOfOPerator]==93)   return valueOfOperand1 .\ valueOfOperand2 ;
-				else if(s[placeOfOPerator]==94)   return valueOfOperand1 .^ valueOfOperand2 ;
-				else if(s.substr(placeOfOPerator+1,placeOfOPerator+3)=="pow(")   
+
+				if(s[placeOfOPerator]==42)        return valueOfOperand1 . dotMul (valueOfOperand2) ;       // **
+				else if(s[placeOfOPerator]==47)   return valueOfOperand1 . dotDiv (valueOfOperand2) ;       // **
+				else if(s[placeOfOPerator]==93)   return valueOfOperand1 . dotLDiv (valueOfOperand2) ;       // **
+				else if(s.substr(placeOfOPerator+1,placeOfOPerator+3)=="pow(" || s[placeOfOPerator]==94)   
 					{
-						size_t const findDouble = s.find_first_of("0123456789");
+						int findDouble = s.find_first_of("0123456789");
 						if (findDouble != std::string::npos)
 						{
-							size_t const endDouble = s.find_first_not_of("0123456789", findDouble);
-							double valueOfOperand2 = stod ( s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble) );
+							int endDouble = s.find_first_not_of("0123456789", findDouble);
+							return valueOfOperand1 . dotPow (stod ( s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble) )) ;                                  //      **
 						}	
-					 return valueOfOperand1 .pow (valueOfOperand2) ;
-
-				    }
-						
+					 
+				
 			}
-		 	
-			
+
 			else if(s[placeOfOPerator]==43 || s[placeOfOPerator]==45 || s[placeOfOPerator]==42 || s[placeOfOPerator]==47 || s[placeOfOPerator]==92 || s[placeOfOPerator]==38 || s[placeOfOPerator]==124)
 			{
-				size_t const findDouble = s.find_first_of("0123456789");  
+				int findDouble = s.find_first_of("0123456789");   
              {
 				if (findDouble != std::string::npos)
 				{
-					size_t const endDouble = s.find_first_not_of("0123456789", findDouble);
+					int endDouble = s.find_first_not_of("0123456789", findDouble);
 					if(findDouble<placeOfOPerator)
 						{
-							double valueOfOperand1 = stod ( s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble) );
+							numberString = s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble) ;
+							valueOfOperand1 = CMatrix ("[" + numberString + "]") ;
 							int operand2index = distance( varNames, find( varNames , varNames + 100 , s.substr(placeOfOPerator+1,s.length()) ) );
-				            CMatrix valueOfOperand2 = CMatrix (varContent[operand2index]);
+				            valueOfOperand2 = CMatrix (varContent[operand2index]);
 					    }
 					else
 						{
-							double valueOfOperand2 = stod ( s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble) );
+							numberString = "[" + s.substr(findDouble, endDouble != std::string::npos ? endDouble-findDouble : endDouble) + "]" ;
+							valueOfOperand2 = CMatrix ("[" + numberString + "]") ;
 							int operand1index = distance( varNames, find( varNames , varNames + 100 , s.substr(0,placeOfOPerator) ) ); 
-				            CMatrix valueOfOperand1 = CMatrix (varContent[operand1index]);
+				            valueOfOperand1 = CMatrix (varContent[operand1index]);
 					    }
-
 				}
  
 				else 
 				{
 					int operand1index = distance( varNames, find( varNames , varNames + 100 , s.substr(0,placeOfOPerator) ) ); 
-				    CMatrix valueOfOperand1 = CMatrix (varContent[operand1index]);					
+					int foundMatrix1 = varContent [operand1index] . find ("[");
+					if(foundMatrix1 != std::string::npos)    valueOfOperand1 = CMatrix (varContent[operand1index]);
+					else 	 valueOfOperand1 = CMatrix ("[" + varContent[operand1index] + "]");
 				
 					int operand2index = distance( varNames, find( varNames , varNames + 100 , s.substr(placeOfOPerator+1,s.length()) ) );
-				    CMatrix valueOfOperand2 = CMatrix (varContent[operand2index]);
+				    int foundMatrix2 = varContent [operand2index] . find ("[");
+					if(foundMatrix2 != std::string::npos)    valueOfOperand2 = CMatrix (varContent[operand2index]);
+					else 	 valueOfOperand2 = CMatrix ("[" + varContent[operand2index] + "]");
 				}
-			}
+			 }
+			  
+			if(s[placeOfOPerator]==43)        return valueOfOperand1 + valueOfOperand2 ;
+		    else if(s[placeOfOPerator]==45)   return valueOfOperand1 - valueOfOperand2 ;
+			else if(s[placeOfOPerator]==42)   return valueOfOperand1 * valueOfOperand2 ;
+			else if(s[placeOfOPerator]==47)   return valueOfOperand1 / valueOfOperand2 ;
+			else if(s[placeOfOPerator]==92)   return valueOfOperand1 . lDiv (valueOfOperand2) ;
+			//else if(s[placeOfOPerator]==38)   return valueOfOperand1 & valueOfOperand2 ;
+			//else if(s[placeOfOPerator]==124)  return valueOfOperand1 | valueOfOperand2 ;
 
-				if(s[placeOfOPerator]==43)        return valueOfOperand1 + valueOfOperand2 ;
-				else if(s[placeOfOPerator]==45)   return valueOfOperand1 - valueOfOperand2 ;
-				else if(s[placeOfOPerator]==42)   return valueOfOperand1 * valueOfOperand2 ;
-				else if(s[placeOfOPerator]==47)   return valueOfOperand1 / valueOfOperand2 ;
-				else if(s[placeOfOPerator]==92)   return valueOfOperand1 \ valueOfOperand2 ;
-				else if(s[placeOfOPerator]==38)   return valueOfOperand1 & valueOfOperand2 ;
-				else if(s[placeOfOPerator]==124)  return valueOfOperand1 | valueOfOperand2 ;
-
 			}
-			
-			
-			
-			
-			 
-		}
+	
+	}
+  }
+	
+		
+					
 
 }
