1cfaee7dfc81ac8eb98fc0ed543433c307ee7849
Marco Younan <xmarcoied@gmail.com>
2018-01-25 20:49:20 +0200

    Errors handler main function: 	stand-alone function to be plugged with the main code 	basic structure 	throw invalid matrices dimension
    Signed-off-by: Marco Younan <xmarcoied@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,69 +1,54 @@
-#include "cmatrix.h"
-#include "solve.h"
-#include "decode.h"
-#include <string>
-#include <fstream>
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(int argc,char* argv[])
-{
-	string varNames[100], varContent[100];
-	int variableNo =0;
-	try {
-		if (argc == 2)
-		{
-		string file = argv[1];
-		ifstream infile(file);
-		string line,info="";
-		int bracketCounter = 0;
-			while (getline(infile, line)) {
-				for (int charIndex = 0; charIndex < line.length(); charIndex++) {
-					if (line[charIndex] == '[') {
-						bracketCounter++;
-					}
-					else if (line[charIndex] == ']') {
-						bracketCounter--;
-					}
-				}
-				info += line;
-				if (bracketCounter == 0 && info!="") {
-					decode(info, varNames, varContent, variableNo);
-					info = "";
 
-				}
-			}
+void errors_handler(string input);
+map<string, pair<string, pair<int,int> > > matrix_data;
+
+int main(){
+	try{
+		string line;
+		cout << ">>"; 
+		while(getline(cin, line)){
+			errors_handler(line);
+			cout << ">>"; 
 		}
-		else
-		{
-		string line, info = "";
-		int bracketCounter = 0;
-		cout << ">>";
-			while (getline(cin, line)) {
-
-				for (int charIndex = 0; charIndex < line.length(); charIndex++) {
-					if (line[charIndex] == '[') {
-						bracketCounter++;
-					}
-					else if (line[charIndex] == ']') {
-						bracketCounter--;
-					}
-				}
-				info += line;
-				if (bracketCounter == 0 && info != "") {
-					decode(info, varNames, varContent, variableNo);
-					info = "";
-
-				}
-				cout << ">>";
-			}
-		}
-		/*cout << "no =" << variableNo<<endl;
-		for (int i = 0; i < variableNo; i++)
-			cout<<varNames[i] << "=" <<varContent[i]<<endl;*/
-		
+	}catch(const char* param){
+		cout << "Error: " << param << "\n";
 	}
-	catch (const char* error) { cout << error<<endl; }
-    return 0;
 }
-
+void errors_handler(string input){
+	// guarantee every semicolon have spaces before and after
+	int index = 0;
+	while (true){
+	     index = input.find(";", index);
+	     if (index == string::npos) break;
+	     input.replace(index, 1, " ; "); index += 3;
+	}
+	
+	char str[1023]; strcpy(str , input.c_str());
+	char *parser = strtok(str , " =");
+	matrix_data[parser].first = input;
+	char *matrix_var = parser;
+	int rows = 1 , cols = -1;
+	set<int> checker;
+	while (true) {
+	    parser = strtok(NULL, " =[]");
+	    if(parser == NULL) break;
+	    cols += 1;
+	    if(!strcmp(parser, ";")){
+	      checker.insert(cols);
+	      rows += 1;
+	      cols = 0;
+	    }
+	}
+	checker.insert(cols);
+	if(checker.size() != 1) throw("Invalid matrix dimension");
+	matrix_data[matrix_var].second.first = rows;
+	matrix_data[matrix_var].second.second = cols;
+	// //show all matrix_data 
+	map<string, pair<string, pair<int,int> > > :: iterator mapit;
+	for(mapit = matrix_data.begin() ; mapit != matrix_data.end() ; mapit++){
+		cout << "matrix(" << mapit->first << ")rows:  " << mapit->second.second.first << ",cols: " << mapit->second.second.second << "\n";
+	}
+}
--- phase2updated/decode.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-using namespace std;
-void print(string s, string varNames[100], string varContent[100], int& variableNo);
-void decode(string s, string varNames[100], string varContent[100], int& variableNo);
-inline bool isInteger(const std::string & s);
-void defMat(string s, string varNames[100], string varContent[100], int& variablesNo);
-void defSpecialMat(string s, string varNames[100], string varContent[100], int& variablesNo);
-void doOperation(string& s, string varNames[100], string varContent[100], int& variablesNo); // Peter /* Will take a string input like A = 3 * sin(0.2) + d (9 + b * exp(3)) where d and b might be matrices*/
