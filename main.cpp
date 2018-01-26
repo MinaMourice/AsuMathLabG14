5b727f19c2d4b7e9386eca7a12127a21ec1b64c7
Marco Younan <xmarcoied@gmail.com>
2018-01-26 21:42:44 +0200

    errors_handler: check variable availability 	cases: 		a = [1] 		b = [1 w] throw error of unavailability
    Signed-off-by: Marco Younan <xmarcoied@gmail.com>

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 void errors_handler(string input);
 void go_space(string &input , char ch);
 bool validate_brackets(string input);
+void matrix_limit(string &input);
+string get_name(string input);
 map<string, pair<string, pair<int,int> > > matrix_data;
 
 int main(){
@@ -21,19 +23,36 @@ int main(){
 }
 void errors_handler(string input){
 	// guarantee every special char have spaces before and after
+	string matrix_name =  get_name(input);
+	matrix_limit(input);
+	matrix_data[matrix_name].first = input;
+	
 	string special_chars = ";][=+-)(/^'";
+	string mat_varNames = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+	size_t mat_found = input.find_first_of(mat_varNames);
+	while (mat_found!=std::string::npos) {
+		string finder = "";
+		finder += input[mat_found];
+		if(matrix_data[finder].second.first == 0) throw("unavailable variable input");
+
+		mat_found=input.find_first_of(mat_varNames,mat_found+1);
+	}
+	cout << input << "\n";
+	input = "a = " + input;
 	if(validate_brackets(input) == false) throw("invalid input");
 	for(int i = 0 ; i < special_chars.size() ; i++){
 		go_space(input , special_chars[i]);
 	}
-	cout << input << "\n";
+
+
+
 	char str[1023]; strcpy(str , input.c_str());
-	char *parser = strtok(str , " =");
-	matrix_data[parser].first = input;
-	char *matrix_var = parser;
+	char *parser = strtok(str , " =[]");
+
 	int rows = 1 , cols = 0;
 	set<int> checker;
 	while (true) {
+
 	    parser = strtok(NULL, " =[]");
 	    if(parser == NULL) break;
 	    cols += 1;
@@ -46,13 +65,14 @@ void errors_handler(string input){
 	}
 	checker.insert(cols);
 	if(checker.size() != 1) throw("Invalid matrix dimension");
-	matrix_data[matrix_var].second.first = rows;
-	matrix_data[matrix_var].second.second = cols;
+	matrix_data[matrix_name].second.first = rows;
+	matrix_data[matrix_name].second.second = cols;
 
 	// //show all matrix_data 
 	map<string, pair<string, pair<int,int> > > :: iterator mapit;
 	for(mapit = matrix_data.begin() ; mapit != matrix_data.end() ; mapit++){
-		cout << "matrix(" << mapit->first << ")rows:  " << mapit->second.second.first << ",cols: " << mapit->second.second.second << "\n";
+		cout << "matrix(" << mapit->first << ")rows: " << mapit->second.second.first << ",cols: " << mapit->second.second.second << " |->";
+		cout << mapit->second.first << "\n";
 	}
 }
 
@@ -93,4 +113,35 @@ bool validate_brackets(string input){
 	}
 
 	return true;
+}
+
+void matrix_limit(string &input){
+	int left = 0 , right = 0;
+	string ret = "";
+	for(int i = 0 ; i < input.size() ; i++){
+		if(input[i] == '['){
+			left = i;
+			break;
+		}
+	}
+	for(int i = 0 ; i < input.size() ; i++){
+		if(input[i] == ']')
+			right = i;
+	}
+	for(int i = left  ; i <= right ; i++)
+		ret += input[i];
+	input = ret;
+}
+
+string get_name(string input){
+	string ret;
+	int count_sign = 0;
+	for(int i = 0 ; i < input.size() ; i++)
+		if(input[i] == '=') count_sign += 1;
+	if(count_sign != 1) throw("invalid input");
+	for(int i = 0 ; i < input.size() ; i++){
+		if(input[i] == '=') break;
+		if(input[i] != ' ') ret += input[i];
+	}
+	return ret;
 }
